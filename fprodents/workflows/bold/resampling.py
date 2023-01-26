88637e75491c667bf96d6d4a26d1997852f8d283
Hyedryn <quentin124@hotmail.be>
2023-01-26 12:15:19 -0500

    Changed template

--- fprodents/workflows/bold/resampling.py
+++ fprodents/workflows/bold/resampling.py
@@ -640,7 +640,7 @@ def _select_template(template):
         return get_template_specs(template, template_spec=specs)[0]
 
     # Map nonstandard resolutions to existing resolutions
-    if template == "MouseIn":
+    if template == "Fischer344":
         default_res = None
     else:
         default_res = 2
