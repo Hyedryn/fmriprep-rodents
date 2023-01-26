8b74d974e14fec790399050013c52f7d2ed8ec8b
Hyedryn <quentin124@hotmail.be>
2023-01-26 17:09:01 -0500

    add dummy svg

--- fprodents/interfaces/confounds.py
+++ fprodents/interfaces/confounds.py
@@ -400,4 +400,6 @@ class FMRISummary(SimpleInterface):
         #import nibabel as nb
         #fig = fMRIPlot( nb.load(self.inputs.in_func).get_fdata(), segments=(self.inputs.in_segm if isdefined(self.inputs.in_segm) else None),tr=self.inputs.tr, confounds=data,units=units,).plot()
         #fig.savefig(self._results["out_file"], bbox_inches="tight")
+        with open(self._results["out_file"], 'w') as creating_new_svg_file:
+            pass
         return runtime
